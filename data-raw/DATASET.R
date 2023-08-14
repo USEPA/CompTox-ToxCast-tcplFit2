624206354379927080bbf3e0d7dee4f5303b33da
brown-jason <42772095+brown-jason@users.noreply.github.com>
2023-08-14 11:53:44 -0400

    5 add standalone example to tcplfit2 vignette (#8)
    * added example mc0 dataset for vignette
    
    * first pass at running example from mc0 to mc5
    
    * first pass at formatting and explanation text
    
    * minor changes to markdown text
    
    * removed extraneous comments
    
    * minor text edits to tcplfit2-vignette.Rmd
    
    Minor text additions to Example 4 of the vignette
    
    * added floating toc and highlighted some terms such as tcpl, tcplfit2, invitrodb
    
    * removed eval = f that was added to speed up knitting
    
    * updates throughout and made it pretty to match other tcpl vignettes
    
    * few more minor edits
    
    * added space after period
    
    * addressing comments
    
    * dependency for tcpl
    
    * added blurb about response value transform
    
    ---------
    
    Co-authored-by: kpaulfriedman <paul-friedman.katie@epa.gov>
    Co-authored-by: Madison Feshuk <feshuk.madison@epa.gov>
    Co-authored-by: cthunes <thunes.carter@epa.gov>

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 signatures <- read.csv(file = "./data-raw/signatures.csv", stringsAsFactors=FALSE)
 usethis::use_data(signatures, overwrite = TRUE)
+
+## For example tcpl data
+#connect to db first
+mc0 <- tcplLoadData(lvl = 0, fld = "acid", val = 1829)
+usethis::use_data(mc0)
