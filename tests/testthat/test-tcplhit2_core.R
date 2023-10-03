b6e26a7afed795fe7732a61c6b47fb765f228238
Zhao <Zhao.Zhihui@epa.gov>
2023-10-03 10:15:05 -0400

    updated tests to meet all requirements in the ticket, added some in-line comments

--- tests/testthat/test-tcplhit2_core.R
+++ tests/testthat/test-tcplhit2_core.R
@@ -17,6 +17,7 @@ test_that("tcplhit2 works", {
 test_that("tcplhit2 BMD upper boundary censor checks", {
   data("mc3")
 
+  # use example data from mc3
   spid <- unique(mc3$spid)[26]
   ex_df <- mc3[is.element(mc3$spid,spid),]
   conc <- 10**ex_df$logc # back-transforming concentrations on log10 scale
@@ -32,17 +33,28 @@ test_that("tcplhit2 BMD upper boundary censor checks", {
   # hit-calling with and without using the BMD boundary argument
   output_before <- tcplhit2_core(params, conc, resp, cutoff, onesd,
                                  bmed=0, conthits=T, aicc=F)
+  # Estimated BMD is greater than the upper 'threshold dose'.
   output_after <- tcplhit2_core(params, conc, resp, cutoff, onesd,
                                 bmed=0, conthits=T, aicc=F, bmd_up_bnd = 2)
+  # Estimated BMD is below the upper 'threshold dose' and
+  # above the 'reference dose' (highest expt dose).
+  output_after_ten <- tcplhit2_core(params, conc, resp, cutoff, onesd,
+                                bmed=0, conthits=T, aicc=F, bmd_up_bnd = 10)
 
+  # checks for bmd estimates
   expect_equal(output_before$bmd, 299.9274, tolerance = 1e-3)
   expect_equal(output_after$bmd, 160, tolerance = 1e-3)
+  expect_equal(output_after_ten$bmd, 299.9274, tolerance = 1e-3)
 
+  # checks for bmd confident lower bound
   expect_equal(output_before$bmdl, 217.343, tolerance = 1e-3)
   expect_equal(output_after$bmdl, 77.41564, tolerance = 1e-3)
+  expect_equal(output_after_ten$bmdl, 217.343, tolerance = 1e-3)
 
+  # checks for bmd confident upper bound
   expect_equal(output_before$bmdu, 475.0638, tolerance = 1e-3)
   expect_equal(output_after$bmdu, 335.1364, tolerance = 1e-3)
+  expect_equal(output_after_ten$bmdu, 475.0638, tolerance = 1e-3)
 })
 
 test_that("tcplhit2 BMD lower boundary censor checks", {
@@ -53,12 +65,11 @@ test_that("tcplhit2 BMD lower boundary censor checks", {
   onesd <- sd(temp)
   cutoff <- 3*bmad
 
-  # load example data
+  # load example data from mc3
   spid <- unique(mc3$spid)[94]
   ex_df <- mc3[is.element(mc3$spid,spid),]
   conc <- 10**ex_df$logc # back-transforming concentrations on log10 scale
   resp <- ex_df$resp
-
   conc2 <- conc[conc>0.41]
   resp2 <- resp[which(conc>0.41)]
 
@@ -68,22 +79,28 @@ test_that("tcplhit2 BMD lower boundary censor checks", {
   # hit-calling with and without using the BMD boundary argument
   output_before <- tcplhit2_core(params, conc2, resp2, cutoff, onesd,
                                  bmed=0, conthits=T, aicc=F)
+  # Estimated BMD is less than the lower 'threshold dose'.
   output_after <- tcplhit2_core(params, conc2, resp2, cutoff, onesd,
                                 bmed=0, conthits=T, aicc=F, bmd_low_bnd = 0.8)
-  output_between <- tcplhit2_core(params, conc2, resp2, cutoff, onesd,
-                                bmed=0, conthits=T, aicc=F, bmd_low_bnd = 0.4)
+  # Estimated BMD is above the lower 'threshold dose' and
+  # below the 'reference dose' (lowest expt dose).
+  output_after_ten <- tcplhit2_core(params, conc2, resp2, cutoff, onesd,
+                                bmed=0, conthits=T, aicc=F, bmd_low_bnd = 0.1)
 
+  # checks for bmd estimates
   expect_equal(output_before$bmd, 0.302, tolerance = 1e-3)
   expect_equal(output_after$bmd, 0.48, tolerance = 1e-3)
-  expect_equal(output_between$bmd, 0.302, tolerance = 1e-3)
+  expect_equal(output_after_ten$bmd, 0.302, tolerance = 1e-3)
 
+  # checks for bmd confident lower bound
   expect_equal(output_before$bmdl, 0.1048, tolerance = 1e-3)
   expect_equal(output_after$bmdl, 0.2826, tolerance = 1e-3)
-  expect_equal(output_between$bmdl, 0.1048, tolerance = 1e-3)
+  expect_equal(output_after_ten$bmdl, 0.1048, tolerance = 1e-3)
 
+  # checks for bmd confident upper bound
   expect_equal(output_before$bmdu, 0.815, tolerance = 1e-3)
   expect_equal(output_after$bmdu, 0.9928, tolerance = 1e-3)
-  expect_equal(output_between$bmdu, 0.815, tolerance = 1e-3)
+  expect_equal(output_after_ten$bmdu, 0.815, tolerance = 1e-3)
 })
 
 test_that("tcplhit2 BMD boundary censor checks", {
@@ -94,7 +111,7 @@ test_that("tcplhit2 BMD boundary censor checks", {
   onesd <- sd(temp)
   cutoff <- 3*bmad
 
-  # find another example
+  # find some example data from mc3
   spid <- unique(mc3$spid)[78]
   ex_df <- mc3[is.element(mc3$spid,spid),]
   conc <- 10**ex_df$logc # back-transforming concentrations on log10 scale
@@ -103,20 +120,25 @@ test_that("tcplhit2 BMD boundary censor checks", {
   params <- tcplfit2_core(conc, resp, cutoff,
                           force.fit = T, bidirectional = F)
 
-  # Estimated BMD is in between the concentration range,
-  # using the boundary
+  # Estimated BMD is in the experimental concentration range
   output_before <- tcplhit2_core(params, conc, resp, cutoff, onesd,
                                  bmed=0, conthits=T, aicc=F)
+
+  # Estimated BMD is below the upper 'reference dose' (highest expt dose)
+  # and is above the lower 'reference dose' (lowest expt. dose).
   output_after <- tcplhit2_core(params, conc, resp, cutoff, onesd,
                                 bmed=0, conthits=T, aicc=F,
                                 bmd_low_bnd = 0.8, bmd_up_bnd = 2)
 
+  # checks for bmd estimates, expect them to be the same
   expect_equal(output_before$bmd, 21.63718, tolerance = 1e-3)
   expect_equal(output_after$bmd, 21.63718, tolerance = 1e-3)
 
+  # checks for bmd confident lower bound, expect them to be the same
   expect_equal(output_before$bmdl, 21.14863, tolerance = 1e-3)
   expect_equal(output_after$bmdl, 21.14863, tolerance = 1e-3)
 
+  # checks for bmd confident upper bound, expect them to be the same
   expect_equal(output_before$bmdu, 22.21793, tolerance = 1e-3)
   expect_equal(output_after$bmdu, 22.21793, tolerance = 1e-3)
 })
