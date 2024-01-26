a6f582cb0cdd625d6337cbfb5b2b2a4051407fd3
gracezhihuizhao <141073347+gracezhihuizhao@users.noreply.github.com>
2024-01-26 10:32:11 -0500

    37 create unit tests for httr signature data fit with tcplfit2 1 (#78)
    * do not track HTTr sub-directory under data-raw/
    
    * Add HTTr unit test data to sysdata.rda
    
    * Script documenting how HTTr unit test data subsets were selected and created
    
    * Added unit test for HTTr signature data
    
    * Added commentary explaining the rationale behind the sampling of additional samples to the subsets.
    
    * Re-ran the script after updating.

--- data-raw/Rinternal_unittest_data.R
+++ data-raw/Rinternal_unittest_data.R
@@ -3,10 +3,15 @@
 ## Pull in HTPP unit test data subsets
 load("~/CompTox-ToxCast-tcplFit2/data-raw/HTPP/htpp_unittest.RData")
 
+## Pull in HTTr unit test data subsets
+load("~/CompTox-ToxCast-tcplFit2/data-raw/HTTr/httr_unittest.RData")
 
 ## Export internal sysdata.rda
 usethis::use_data(htpp_global_subset, htpp_cat_subset, htpp_feature_subset,
                   htpp_global_input, htpp_cat_input, htpp_feature_input,
-                  CONTROL_GMAH, CONTROL_CMAH, CONTROL_FMAH, internal = TRUE, overwrite = T)
+                  CONTROL_GMAH, CONTROL_CMAH, CONTROL_FMAH,
+                  signature_sub, gene_sub,
+                  signature_input, gene_input,
+                  internal = TRUE, overwrite = T)
 ## Include session info
 utils::sessionInfo()
