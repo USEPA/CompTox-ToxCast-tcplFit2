56d29f54ee221c84a410fe4cdb992dd762fafd00
gracezhihuizhao <141073347+gracezhihuizhao@users.noreply.github.com>
2024-02-12 09:54:54 -0500

    26 update plotting functionality within tcplfit2 r package (#77)
    * Drafted ggplot version of do.plot option in tcplfit2_core, and ggplot version of concRespPlot.
    
    * Added documentation, updated the code to meet the needs in the issue.
    
    * Drafted a vignette for the new plotting function, showing how to add more ggplot layers on top of the plot
    
    * saved the codes I have for plotting all curves - finish later
    
    * finished implementing the functionality and finished with the documentation
    
    * Added plot_allcurves function to the vignette, show log concentration argument
    
    * polished the text
    
    * Updated the function to plot the models that were able to fit and throw a message about any failed models
    
    * Updated the concentration series, replace 0 with 1e-10 if it's in the concentration series to prevent error when log is used
    
    * grammar fix
    
    * simplified the code flow, edited the documentations
    
    * export two new functions
    
    * Edited the text in the vignette
    
    * Edits to the figure captions
    
    * Changed the order of some codes.
    
    * Show what the input objects going into the plotting functions look like; Edited the texts.
    
    * minor documentation updates to the new plotting functions - concRespPlot2 and plot_allcurves
    
    * updated text in the plotting vignette provide additional context and clarity, and fix typos/grammar issues
    
    * fixed figure number
    
    * Handling of 0 concentration when log-10 concentration is used
    
    * minor updates to the warning message when conc = 0 exists and log_conc argument is set to TRUE - new plotting functions
    
    ---------
    
    Co-authored-by: Sarah Davidson-Fritz <davidsonfritz.sarah@epa.gov>

---
+++ R/plot_allcurves.R
@@ -0,0 +1,83 @@
+#' Plot All Curves Fit with tcplfit2_core - ggplot2
+#'
+#' This function takes output from `tcplfit2_core` and generates a basic plot of
+#' the observed concentration-response data with all resulting curve fits.
+#' A `ggplot` object, which users may customize with additional `ggplot` layers,
+#' is returned.
+#'
+#' @param modelfits Output from `tcplfit2_core`, contains resulting fits for all
+#' models used to evaluate the observed concentration-response data.
+#'
+#' @param conc Vector of concentrations (NOT in log units).
+#'
+#' @param resp Vector of responses.
+#'
+#' @param log_conc Logical argument. If `TRUE`, convert the concentrations (x-axis)
+#' into log-10 scale. Defaults to `FALSE`.
+#'
+#' @return A `ggplot` object of the observed concentration-response data and
+#' all resulting curve fits from `tcplfit2_core`. (Note: The constant model is
+#' not included, and only the successful fits will be displayed.)
+#'
+#' @export
+plot_allcurves <- function(modelfits, conc, resp, log_conc = FALSE) {
+
+  list2env(modelfits, envir = environment())
+
+  shortnames <- modelnames[modelnames != "cnst"]
+  successes <- sapply(shortnames, function(x) {
+    get(x)[["success"]]
+  })
+
+  # If any of the models from the output failed,
+  # display the models that do fit and throw a warning message
+  # about the failed models being excluded from the plot.
+  if (sum(successes, na.rm = T) != length(shortnames)) {
+    failed_fits <- names(which(successes != 1))
+    shortnames <- shortnames[! shortnames %in% failed_fits]
+    warning(paste0("The following model(s) failed to fit: ", paste(failed_fits, collapse = ', '),
+                  ", and are excluded from the plot.\n"))
+  }
+
+
+  X <- seq(min(conc), max(conc), length.out = 100)
+  allresp <- NULL
+  pars_lists <- sapply(shortnames, function(x) {get(x)[["pars"]]})
+
+  for (i in 1:length(pars_lists)) {
+    model <- names(pars_lists)[i]
+    values <- sapply(pars_lists[[model]], function(x) {
+      get(model)[[x]]})
+    if (model == "hill") model <- paste0(model,"fn")
+    y <- do.call(model, list(values, X))
+    allresp <- cbind(allresp, y)
+  }
+
+  colnames(allresp) <- shortnames
+
+  if (log_conc) {
+    if (any(conc==0)) warning("Data contains untreated controls (conc = 0). A pseudo value replaces -Inf after log-transform. The pseudo value is set to one log-unit below the lowest experimental `conc`.")
+    conc <- log10(conc)
+    conc <- replace(conc, conc==-Inf, sort(conc)[2]-1)
+    X <- log10(X)
+    X <- replace(X, X==-Inf, sort(conc)[2]-1)
+  }
+
+  # Format data into a data.frame for ease of plotting.
+  estDR <- cbind.data.frame(X,allresp) %>%
+    reshape2::melt(data = .,measure.vars = shortnames)
+
+  ## Plot the Model Fits ##
+  p <- ggplot(data.frame(conc, resp), aes(x = conc,y = resp)) +
+    geom_point(pch = 1,size = 2)+
+    geom_line(data = estDR,
+              aes(x = X,y = value,colour = variable,lty = variable))+
+    labs(colour = "Models",lty = "Models")+
+    xlab("Concentration")+
+    ylab("Responses")+
+    theme_bw()
+
+  if (log_conc) p <- p + xlab(expression(paste(log[10],"(Concentration) ",mu,"M")))
+
+  return(p)
+}
