3ac1c83f117d9395e963e2276456cb3b0299b288
knuppla <knupp.lindsay@epa.gov>
2024-11-26 08:06:23 -0500

    changed brewer.pal color palette

--- R/tcplfit2_core.R
+++ R/tcplfit2_core.R
@@ -141,7 +141,7 @@ tcplfit2_core <- function(conc, resp, cutoff, force.fit = FALSE, bidirectional =
   if (do.plot && sum(successes, na.rm = T) == length(shortnames)) {
     resp <- resp[order(logc)]
     #par(xpd = T)
-    cols <- c("black", brewer.pal(9, "Set1"))
+    cols <- c("black","darkred", brewer.pal(8, "Dark2"))
     n <- length(logc)
     allresp <- c(resp, sapply(shortnames, function(x) {
       get(x)[["modl"]][order(logc)]
