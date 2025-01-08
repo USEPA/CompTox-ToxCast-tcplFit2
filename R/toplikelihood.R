ee56603c40187554a94420609f789f8a51f85ace
knuppla <knupp.lindsay@epa.gov>
2025-01-08 13:17:24 -0500

    comment code

--- R/toplikelihood.R
+++ R/toplikelihood.R
@@ -42,6 +42,8 @@ toplikelihood = function(fname, cutoff, conc, resp, ps, top, mll, errfun = "dt4"
   conc_seq = 10**seq(from = log10(min(conc)), to = log10(max(conc)), by = 0.05)
 
   #reparameterize so that top is exactly at cutoff
+  #for each method, first calculate pred: predicted curve
+  #all methods explicitly solve for conc where top occurs
   if(fname == "exp2"){
     pred = do.call(fname,list(c(ps[1],ps[2],ps[3]),conc_seq))
     ps[1] = cutoff/( exp(conc_seq[which.max(abs(pred))]/ps[2]) - 1 )
@@ -51,25 +53,19 @@ toplikelihood = function(fname, cutoff, conc, resp, ps, top, mll, errfun = "dt4"
   } else if(fname == "exp4"){
     pred = do.call(fname,list(c(ps[1],ps[2],ps[3]),conc_seq))
     ps[1] = cutoff/( 1 - 2^(-conc_seq[which.max(abs(pred))]/ps[2]))
-    # ps[1] = cutoff
   } else if(fname == "exp5"){
     pred = do.call(fname,list(c(ps[1],ps[2],ps[3],ps[4]),conc_seq))
     ps[1] = cutoff/( 1 - 2^(-(conc_seq[which.max(abs(pred))]/ps[2])^ps[3]))
-    # ps[1] = cutoff
   } else if(fname == "hillfn"){
     pred = do.call(fname,list(c(ps[1],ps[2],ps[3],ps[4]),conc_seq))
     ps[1] = cutoff * ( 1 + (ps[2]/conc_seq[which.max(abs(pred))])^ps[3])
-    # ps[1] = cutoff
   } else if(fname == "gnls"){
-    #approximating actual top with theoretical top for convenience.
     pred = do.call(fname,list(c(ps[1],ps[2],ps[3],ps[4],ps[5],ps[6]),conc_seq))
     ps[1] = cutoff * (( 1 + (ps[2]/conc_seq[which.max(abs(pred))])^ps[3])*( 1 + (conc_seq[which.max(abs(pred))]/ps[4])^ps[5]))
-    # ps[1] = cutoff
   } else if(fname == "poly1"){
     pred = do.call(fname,list(c(ps[1],ps[2]),conc_seq))
     ps[1] = cutoff/conc_seq[which.max(abs(pred))]
   } else if(fname == "poly2"){
-    #fit curve to find max predicted response
     pred = do.call(fname,list(c(ps[1],ps[2],ps[5]),conc_seq))
     ps[1] = cutoff/(conc_seq[which.max(abs(pred))]/ps[2] + (conc_seq[which.max(abs(pred))]/ps[2])^2 )
   } else if(fname == "pow"){
